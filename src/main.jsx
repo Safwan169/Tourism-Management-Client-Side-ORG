abee94d012ca629c0179382e28157ebd6b8e00f3
safwan 169 <safwanhossainmridul@gmail.com>
2024-04-30 22:42:36 +0600

    tew

--- src/main.jsx
+++ src/main.jsx
@@ -69,7 +69,7 @@ const router = createBrowserRouter([
         element:<PrivateRoute><Updatae></Updatae></PrivateRoute>
       },{
         path:"/country/:Country",
-        loader:()=>fetch(`https://b9a10-server-side-safwan169.vercel.app//country`),
+        loader:()=>fetch(`https://b9a10-server-side-safwan169.vercel.app/country`),
 
         // loader:({params})=>fetch(`https://b9a10-server-side-safwan169-mwvtb8ury-safwan-hossains-projects.vercel.app/country/${params.Country}`),
 
